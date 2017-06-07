5a7400adb57189721536325b33e2abbde376a860
Katy Morris <katymcconica@gmail.com>
2017-06-07 00:46:34 -0700

    adjusted project box heights, fixed height and width issues with photos

--- js/main.js
+++ js/main.js
@@ -67,7 +67,8 @@ function positionProductBoxes() {
 		var box = $(this).find('.product-box');
 		var margin = (sectionHeight - $(box).height());
 		if (windowWidth > 767 && windowWidth < 1200) {
-			$(box).css('margin-top', margin / 2);
+			var sectionTitleHeight = $('.section-1 .section-title').height();
+			$(box).css('margin-top', (margin / 2) - sectionTitleHeight);
 		} else {
 			$(box).attr('style', ' "" ');
 		} 
@@ -110,24 +111,24 @@ $('body').on('mouseleave', ".project-link-btn ", function() {
 	}
 });
 
-$("a").on('click', function(event) {
-	debugger
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
-    // Prevent default anchor click behavior
-    event.preventDefault();
+// $("a").on('click', function(event) {
+// 	debugger
+//   // Make sure this.hash has a value before overriding default behavior
+//   if (this.hash !== "") {
+//     // Prevent default anchor click behavior
+//     event.preventDefault();
 
-    // Store hash
-    var hash = this.hash;
+//     // Store hash
+//     var hash = this.hash;
 
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-    });
-  } // End if
-});
+//     // Using jQuery's animate() method to add smooth page scroll
+//     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//     $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){
+//       // Add hash (#) to URL when done scrolling (default click behavior)
+//       window.location.hash = hash;
+//     });
+//   } // End if
+// });
 
  //skill box hovers
 $('body').on('mouseenter', '.skill-box', function() {
